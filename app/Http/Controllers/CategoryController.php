88951169f1a48774259f835d79ab9da67f9a4b17
nhoxbin <khoaij123@gmail.com>
2020-08-15 15:49:10 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+class CategoryController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function index() {
+    	
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,15 @@
 
 class Controller
 {
+	const VIEW_FOLDER = 'resources/views';
+
     public function __construct()
     {
         
+    }
+
+    protected function view($path) {
+    	$path = self::VIEW_FOLDER . '/' . str_replace('.', '/', $path) . '.php';
+    	require $path;
     }
 }
